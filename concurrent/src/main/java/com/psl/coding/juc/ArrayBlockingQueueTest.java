13314d7651c4f1e5592abdbc8a9baf80f6db8ed0
psl <782683767@qq.com>
2025-10-12 21:50:48 +0800

    阻塞队列

--- concurrent/src/main/java/com/psl/coding/juc/ArrayBlockingQueueTest.java
+++ concurrent/src/main/java/com/psl/coding/juc/ArrayBlockingQueueTest.java
@@ -1,6 +1,13 @@
 package com.psl.coding.juc;
 
+import java.util.concurrent.ArrayBlockingQueue;
+
 /**
+ *
+ *
+ * add 注意队列满会报异常
+ * offer 队列满返回false，也有阻塞的方法
+ * put 阻塞的，
  * ArrayBlockingQueue，使用数组实现，具有容量限制
  * 一个数组，
  * 一把锁，
@@ -10,6 +17,6 @@ package com.psl.coding.juc;
  * */
 public class ArrayBlockingQueueTest {
     public static void main(String[] args) {
-
+        ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(100);
     }
 }
